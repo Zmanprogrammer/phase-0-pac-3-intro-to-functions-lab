fe5b5ed579179f76e28abfb415c18f0d7a3bc1a0
Jamir <championjamir@gmail.com>
2022-11-24 21:37:57 -0500

    commit tree 4617b8e48e0aa97715b2b6e19f83cd88d9ee6f0a

---
+++ index.js
@@ -0,0 +1,29 @@
+
+ function shout(string) {
+    return string.toUpperCase()
+ }
+
+
+ function whisper(string) {
+    return string.toLowerCase()
+ }
+ function logShout(string) {
+    string.toUpperCase()
+    console.log("HELLO")
+ }
+string = "hello"
+function logWhisper(string) {
+    string.toLowerCase()
+    console.log("hello")
+}
+
+
+function sayHiToHeadphonedRoommate(string) {
+   if (string === string.toUpperCase()) return "YES INDEED!"
+   if (string === string.toLowerCase()) return  'I can\'t hear you!'
+   if (string === 'Let\'s have dinner together!') return "I would love to!"
+}
+
+
+
+
